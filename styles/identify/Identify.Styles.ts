ac7e07384e0a250c20730424f6fb8c7e000bd589
Ewerton G <tonpublic97@gmail.com>
2023-11-19 21:06:14 -0300

    add identification Style

---
+++ styles/identify/Identify.Styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f0f0f7;
+  padding: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  color: #333;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 1rem;
+  color: #666;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+export const ImageContainer = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+export const RoleContainer = styled.div`
+  justify-content: center;
+  display: flex;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+
+export const RoleButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  background-color: #f7f7f7;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 2rem;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  color: #333;
+  margin-right: 1rem;
+
+  &:active {
+    background-color: #e7e7e7;
+  }
+  &:hover {
+    border: 3px solid #ccc;
+  }
+`;
+
+export const Icon = styled.img`
+  margin-bottom: 1rem;
+`;
+
+export const NextButton = styled.button`
+  background-color: #2E46B7;
+  color: white;
+  padding: 1rem 2rem;
+  border-radius: 10px;
+  border: none;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: #6880F3;
+  }
+`;
+
+// Adicione quaisquer outros estilos necessários para sua página aqui.
