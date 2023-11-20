033add30b1da7fc030a8ee16e6c54cd62ae5291e
Ewerton G <tonpublic97@gmail.com>
2023-11-20 00:09:26 -0300

    register pages

---
+++ src/app/identify/register/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react';
+import {
+  RegistrationContainer,
+  Title,
+  Form,
+  Input,
+  CheckboxContainer,
+  Checkbox,
+  Button,
+  LinkText,
+  LinkButton
+} from '../../../../styles/identify/register/Register.Styles';
+
+export default function Signup() {
+  return (
+    <RegistrationContainer>
+      <Title>Registro</Title>
+      <Form>
+        <Input type="text" placeholder="Nome" />
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Senha" />
+        <CheckboxContainer>
+          <Checkbox type="checkbox" id="terms" />
+          <label htmlFor="terms">Concordo com os termos e condições.</label>
+        </CheckboxContainer>
+        <Button type="submit">Registrar</Button>
+      </Form>
+      <LinkText>
+        Já possui uma conta? <LinkButton href="/login">Faça Login</LinkButton>
+      </LinkText>
+    </RegistrationContainer>
+  );
+}
