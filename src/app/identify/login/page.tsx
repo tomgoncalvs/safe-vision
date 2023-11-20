0f5c897bab7243a362c35c195531ca3201a1cb59
Ewerton G <tonpublic97@gmail.com>
2023-11-19 21:06:41 -0300

    Add Register & Doc Register page

---
+++ src/app/identify/login/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react';
+import {
+  LoginContainer,
+  Title,
+  Form,
+  Input,
+  Button,
+  LinkText,
+  LinkButton
+} from '../../../../styles/identify/register/Register.Styles'; // Atualize o caminho conforme necessário
+
+export default function Login() {
+  return (
+    <LoginContainer>
+      <Title>Entre</Title>
+      <Form>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
+      </Form>
+      <LinkText>
+        Ainda não tem uma conta? <LinkButton href="/signup">Crie agora!</LinkButton>
+      </LinkText>
+    </LoginContainer>
+  );
+}
